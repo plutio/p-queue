8a37262f31e7438289e00b7258304c238a783171
Ihor Vasylechko <igorwas29091995@gmail.com>
2025-05-07 11:33:42 +0300

    commited builded version of 8.1.0

--- test-d/index.test-d.ts
+++
@@ -1,7 +0,0 @@
-import {expectType} from 'tsd';
-import PQueue from '../source/index.js';
-
-const queue = new PQueue();
-
-expectType<Promise<string | void>>(queue.add(async () => '🦄'));
-expectType<Promise<string>>(queue.add(async () => '🦄', {throwOnTimeout: true}));
